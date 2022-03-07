260f70918a481d43c13d81e0d8dff598a3fa7a35
root <saung.myat@gmail.com>
2022-03-06 20:05:37 -0500

    commit tree d408fdbdcac43ccd813b5ab9d5d285c6b443307b

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function superbowlWin(record) {
+    for (const win of record) {
+        if (win.result === 'W') {
+        return win.year;
+        }
+
+    }
+
+
+}
+    
+
+record.find(superbowlWin);
